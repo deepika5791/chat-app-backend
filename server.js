6f0160638220e377aed92d2c321e01f3d30c7d68
deepika5791 <deepikatripathi0810@gmail.com>
2025-10-15 20:18:15 +0530

    added timestaps

--- server.js
+++ server.js
@@ -45,5 +45,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
