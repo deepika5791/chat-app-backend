1f4aefa2c382da5fb9f61a159be1d105d03d4300
deepika5791 <deepikatripathi0810@gmail.com>
2025-10-17 12:12:42 +0530

    removed some

--- src/useController/authController.js
+++ src/useController/authController.js
@@ -13,7 +13,6 @@ const signup = async (req, res) => {
       return res.status(400).json({ error: "User already exists" });
     }
 
-    // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
