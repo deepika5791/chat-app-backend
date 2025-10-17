989e9a7a7fc982ac9b7f2f15762fd4dfba2f3bcf
deepika5791 <deepikatripathi0810@gmail.com>
2025-10-17 15:14:53 +0530

    changed codes

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ const io = new Server(server, {
     credentials: true,
   },
   allowEIO3: true, // important for WSS in some deployments
-  transports: ["websocket", "polling"],
+  transports: ["polling"],
 });
 
 app.set("io", io);
