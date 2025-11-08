cfde16920a67a5e6be8ef1916f81475a6572f883
deepika5791 <deepikatripathi0810@gmail.com>
2025-11-08 18:42:13 +0530

    added get request

--- src/userRoutes/authRoutes.js
+++ src/userRoutes/authRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { signup, login, saveProfile } = require("../useController/authController");
 
+router.get("/uptime", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/save-profile", saveProfile);
